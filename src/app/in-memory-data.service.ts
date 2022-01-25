15696b72827944d746c396a4bfa80d64e2c64591
Huni Lee(이지훈) <jhlee6@bistelligence.ai>
2022-01-25 17:20:58 +0900

    v1.1

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -4,16 +4,26 @@ import { Hero } from './hero';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const heroes = [
-      { id: 11, name: 'Mr. Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 1, name: 'Mr. Nice' },
+      { id: 2, name: 'Narco' },
+      { id: 3, name: 'Bombasto' },
+      { id: 4, name: 'Celeritas' },
+      { id: 5, name: 'Magneta' },
+      { id: 6, name: 'RubberMan' },
+      { id: 7, name: 'Dynama' },
+      { id: 8, name: 'Dr IQ' },
+      { id: 9, name: 'Magma' },
+      { id: 10, name: 'Tornado' },
+      { id: 11, name: 'Huni' },
+      { id: 12, name: 'Yone' },
+      { id: 13, name: 'Yasuo' },
+      { id: 14, name: 'Jax' },
+      { id: 15, name: 'Lee sin' },
+      { id: 16, name: 'Ezreal' },
+      { id: 17, name: 'Stone' },
+      { id: 18, name: 'Gwen' },
+      { id: 19, name: 'Renekton' },
+      { id: 20, name: 'Tristana' }
     ];
     return { heroes };
   }
