b455b8427c391379621026ce76151377e65cc8a5
Huni Lee(이지훈) <jhlee6@bistelligence.ai>
2022-01-27 10:55:33 +0900

    clock

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
 
@@ -8,13 +7,27 @@ import { HeroService } from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
+
 export class HeroesComponent implements OnInit {
   heroes!: Hero[];
+  today!: Date;
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService) {}
 
   ngOnInit() {
     this.getHeroes();
+  }
+
+  getTime(): void {
+    this.today = new Date();
+    var hur = this.today.getHours();		// 시
+		var min = this.today.getMinutes();	//분
+		var sec = this.today.getSeconds();	//초
+    var timeBoard = document.getElementById("test");
+    var time = "현재 시간 : " + hur + "시 " + min + "분 " + sec + "초";
+    timeBoard!.innerHTML = time;
+    setInterval(this.getTime, 1000);
   }
 
   getHeroes(): void {
